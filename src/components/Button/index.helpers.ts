a8253af393266f06954e5944bc6e3d40e37e141c
Ezra Audivano Dirfa <ezra@skyshi.com>
2023-05-05 20:17:56 +0700

    style: button count me in hover

--- src/components/Button/index.helpers.ts
+++ src/components/Button/index.helpers.ts
@@ -7,7 +7,7 @@ export const getButtonStyleByVariant = (variant: ButtonVariant) => {
       return clsx('bg-blue text-white hover:opacity-70 transition-all');
     }
     case 'secondary': {
-      return clsx('bg-orange text-[12px] w-[120px] !p-2 font-bold h-[32px] px-6 text-white hover:bg-white hover:text-dark-blue transition-all');
+      return clsx('bg-orange text-[12px] w-[120px] !p-2 font-bold h-[32px] px-6 text-white hover:bg-white hover:text-dark-orange transition-all');
     }
     case 'transparent': {
       return clsx('bg-transparent border-2 font-bold h-[54px] px-6 text-white hover:bg-white hover:text-dark-blue transition-all');
